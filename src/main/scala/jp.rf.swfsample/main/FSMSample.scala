d43e8cc5ba4440537f1cbc7d92dfd76e28c645e0
fujisawa_r <fujisawa_r@worksap.co.jp>
2014-05-14 19:15:31 +0900

    FSM sample - wrap state-transition definition

--- src/main/scala/jp.rf.swfsample/main/FSMSample.scala
+++ src/main/scala/jp.rf.swfsample/main/FSMSample.scala
@@ -2,9 +2,10 @@ package jp.rf.swfsample.main
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.reflect.{ClassTag, classTag}
 
 import akka.actor.ActorDSL.{Act, actor => act}
-import akka.actor.{ActorRef, ActorSystem, FSM}
+import akka.actor.{ActorRef, ActorRefFactory, ActorSystem, FSM}
 import akka.pattern.ask
 import akka.util.Timeout
 
@@ -24,6 +25,7 @@ object FSMSample {
   case object Stop extends Action
   case class Polled(task: Option[Task]) extends Action
   case object Done extends Action
+  case object GetState extends Action
   
   sealed trait ActualAction
   case object Poll extends ActualAction
@@ -31,89 +33,116 @@ object FSMSample {
   
   def main(args: Array[String]) {
     val system = ActorSystem()
-    val actor = act(system)(new Act with FSM[State, Unit] {
-      val actionActor = act(context)(new Act {
-        become {
-          case Poll => {
-            println("polling")
-            Thread.sleep(500)
-            println("polled")
-            sender ! Polled(Some(Task("hoge")))
+    val actionActor = act(system)(new Act {
+      become {
+        case Poll => {
+          println("polling")
+          Thread.sleep(500)
+          println("polled")
+          sender ! Polled(Some(Task("hoge")))
+        }
+        case Execute(Task(name)) => {
+          println("working - " + name)
+          Thread.sleep(500)
+          println("done")
+          sender ! Done
+        }
+      }
+    })
+    val actor = createActor(actionActor)(system)
+    implicit val timeout = Timeout(FiniteDuration(5, SECONDS))
+    actor ! Start
+    Thread.sleep(1000)
+    println(Await.result(actor ? GetState, timeout.duration))
+    Thread.sleep(1500)
+    println(Await.result(actor ? GetState, timeout.duration))
+    Thread.sleep(1500)
+    println(Await.result(actor ? GetState, timeout.duration))
+    actor ! Stop
+    println(Await.result(actor ? GetState, timeout.duration))
+    Thread.sleep(1000)
+    println(Await.result(actor ? GetState, timeout.duration))
+    actor ! Start
+    Thread.sleep(1000)
+    println(Await.result(actor ? GetState, timeout.duration))
+    Thread.sleep(1400)
+    println(Await.result(actor ? GetState, timeout.duration))
+    actor ! Stop
+    println(Await.result(actor ? GetState, timeout.duration))
+    Thread.sleep(1500)
+    println(Await.result(actor ? GetState, timeout.duration))
+    system.shutdown
+  }
+  def createActor(activity: ActorRef)(implicit factory: ActorRefFactory): ActorRef = {
+    FSMActor.create(new FSMActorConf[State, Action] {
+      val states = Seq(
+        Inactive,
+        Active(Polling, true),
+        Active(Polling, false),
+        Active(Working, true),
+        Active(Working, false)
+      )
+      val initialState = Inactive
+      override def transition(state: State, event: Action) = (state, event) match {
+        case (Inactive,               Start)           => Active(Polling, true)
+        case (Active(Polling, cont),  Polled(Some(_))) => Active(Working, cont)
+        case (Active(Polling, true),  Stop)            => Active(Polling, false)
+        case (Active(Polling, false), Polled(None))    => Inactive
+        case (Active(Polling, false), Start)           => Active(Polling, true)
+        case (Active(Working, true),  Done)            => Active(Polling, true)
+        case (Active(Working, true),  Stop)            => Active(Working, false)
+        case (Active(Working, false), Done)            => Inactive
+        case (Active(Working, false), Start)           => Active(Working, true)
+        case (status,                 _)               => status
+      }
+      override def action(state: State, event: Action, actor: FSMAct) {
+        implicit val sender = actor.self
+        (state, event) match {
+          case (Inactive,              Start)              => activity ! Poll
+          case (Active(Polling, true), Polled(None))       => activity ! Poll
+          case (Active(Polling, _),    Polled(Some(task))) => activity ! Execute(task)
+          case (Active(Working, true), Done)               => activity ! Poll
+          case (_,                     GetState)           => actor.sender ! actor.stateName
+          case _                                           => 
+        }
+      }
+    })
+  }
+}
+
+trait FSMActorConf[S, E] {
+  type FSMAct = Act with FSM[S, Unit]
+  val states: Seq[S]
+  val initialState: S
+  def transition(state: S, event: E): S
+  def action(state: S, event: E, actor: FSMAct): Unit
+  def unhandledTransition(state: S, event: Any): Option[S] = None
+  def unhandledAction(state: S, event: Any, actor: FSMAct): Unit = {}
+}
+
+object FSMActor {
+  def create[S, E: ClassTag](conf: FSMActorConf[S, E])(implicit factory: ActorRefFactory): ActorRef = {
+    act(factory)(new Act with FSM[S, Unit] {
+      startWith(conf.initialState, ())
+      for (state <- conf.states) {
+        when(state) {
+          case Event(anyE, _) => safeCast[E](anyE) match {
+            case None => {
+              conf.unhandledAction(state, anyE, this)
+              conf.unhandledTransition(state, anyE).map(goto).getOrElse(stay)
+            }
+            case Some(e) => {
+              conf.action(state, e, this)
+              goto(conf.transition(state, e))
+            }
           }
-          case Execute(Task(name)) => {
-            println("working - " + name)
-            Thread.sleep(500)
-            println("done")
-            sender ! Done
-          }
-        }
-      })
-      startWith(Inactive, ())
-      when(Inactive) {
-        case Event(Start, _) => {
-          actionActor ! Poll
-          goto(Active(Polling, true))
-        }
-      }
-      when(Active(Polling, true)) {
-        case Event(Polled(None), data) => {
-          actionActor ! Poll
-          stay
-        }
-        case Event(Polled(Some(task)), _) => {
-          actionActor ! Execute(task)
-          goto(Active(Working, true))
-        }
-        case Event(Stop, _) => goto(Active(Polling, false))
-      }
-      when(Active(Polling, false)) {
-        case Event(Polled(None), _) => goto(Inactive)
-        case Event(Polled(Some(task)), _) => {
-          actionActor ! Execute(task)
-          goto(Active(Working, false))
-        }
-        case Event(Start, _) => goto(Active(Polling, true))
-      }
-      when(Active(Working, true)) {
-        case Event(Done, _) => {
-          actionActor ! Poll
-          goto(Active(Polling, true))
-        }
-        case Event(Stop, _) => goto(Active(Working, false))
-      }
-      when(Active(Working, false)) {
-        case Event(Done, _) => goto(Inactive)
-        case Event(Start, _) => goto(Active(Working, true))
-      }
-      whenUnhandled {
-        case Event('state, _) => {
-          sender ! stateName
-          stay
         }
       }
       initialize
     })
-    implicit val timeout = Timeout(FiniteDuration(5, SECONDS))
-    actor ! Start
-    Thread.sleep(1000)
-    println(Await.result(actor ? 'state, timeout.duration))
-    Thread.sleep(1500)
-    println(Await.result(actor ? 'state, timeout.duration))
-    Thread.sleep(1500)
-    println(Await.result(actor ? 'state, timeout.duration))
-    actor ! Stop
-    println(Await.result(actor ? 'state, timeout.duration))
-    Thread.sleep(1000)
-    println(Await.result(actor ? 'state, timeout.duration))
-    actor ! Start
-    Thread.sleep(1000)
-    println(Await.result(actor ? 'state, timeout.duration))
-    Thread.sleep(1400)
-    println(Await.result(actor ? 'state, timeout.duration))
-    actor ! Stop
-    println(Await.result(actor ? 'state, timeout.duration))
-    Thread.sleep(1500)
-    println(Await.result(actor ? 'state, timeout.duration))
-    system.shutdown
+  }
+  def safeCast[A: ClassTag](x: Any): Option[A] = {
+    val cls = classTag[A].runtimeClass.asInstanceOf[Class[A]]
+    if (cls.isInstance(x)) Some(cls.cast(x)) else None
   }
 }
